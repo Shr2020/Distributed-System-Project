a2b965598b43870b5a9194effd7d6839c486a200
Sravani Namburi <sravaninamburi@Sravanis-MacBook-Pro.local>
2021-12-06 04:24:55 -0500

    added probability metrics

--- apps/project/test/Dynamo_test.exs
+++ apps/project/test/Dynamo_test.exs
@@ -9,21 +9,19 @@ defmodule DynamoTest do
   test "measure  reads after write probabilty" do
     Emulation.init()
     
-    Emulation.append_fuzzers([Fuzzers.delay(100)])
-    view = [:a, :b, :c, :d, :e]
+    Emulation.append_fuzzers([Fuzzers.delay(50)])
+    view = [:a, :b, :c]
     base_config =
       Dynamo.new_configuration(view, 1, 1, 20_000, 30_000)
 
     spawn(:b, fn -> Dynamo.become_replica(base_config) end)
     spawn(:c, fn -> Dynamo.become_replica(base_config) end)
     spawn(:a, fn -> Dynamo.become_replica(base_config) end)
-    spawn(:d, fn -> Dynamo.become_replica(base_config) end)
-    spawn(:e, fn -> Dynamo.become_replica(base_config) end)
 
     client =
       spawn(:client, fn ->
         client = Dynamo.Client.new_client(:client)
-        measure_read_consistency(view,100,0)  
+        measure_read_consistency(view,50,0)  
          
     end)
 
@@ -48,17 +46,54 @@ defmodule DynamoTest do
         IO.puts("selected randomserver #{inspect(random_server)}\n")
         val = generate_random_val()
         send(random_server,{:set,"p", val})
-        Emulation.timer(800,:RL)
+        Emulation.timer(200,:RL)
         read = measure_monotonic_reads(view,"p", val)
     measure_read_consistency(view,count-1,updated+read) 
   else
   IO.puts("probability#{inspect(updated/100)}\n")
-  #values for fixed 100ms delay
-  # after 200ms - 0.83 probability
+  #values for fixed 100ms delay for R=1, W=1
+  # after 200ms - 0.8 probability
   # after 500ms - 0.97 probability
   # after 800ms - 0.98 probability
   # after 1200ms - 1.0 probability
   # after 1500ms - 1.0 probability
+
+  #values for fixed 100ms delay for R=1, W=2
+  # after 200ms - 0.82 probability
+  # after 500ms - 0.98 probability
+  # after 800ms - 1.0 probability
+  # after 1200ms - 1.0 probability
+  # after 1500ms - 1.0 probability
+
+  #values for fixed 100ms delay for R=2, W=1
+  # after 200ms - 0.77 probability
+  # after 500ms - 0.98 probability
+  # after 800ms - 1.0 probability
+  # after 1200ms - 1.0 probability
+  # after 1500ms - 1.0 probability
+#------ R and w fixed to 1
+  #values for fixed 100ms delay for N=3
+  # after 200ms - 
+  # after 500ms - 
+  # after 800ms - 
+  # after 1200ms - 
+  # after 1500ms - 
+
+  #values for fixed 100ms delay for N=4
+  # after 200ms - 
+  # after 500ms -
+  # after 800ms - 
+  # after 1200ms - 
+  # after 1500ms - 
+
+  #values for fixed 100ms delay for N=5
+  # after 200ms - 
+  # after 500ms - 
+  # after 800ms - 
+  # after 1200ms - 
+  # after 1500ms - 
+
+
   end
   end
 
